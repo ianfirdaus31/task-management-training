caf7f18f458532258e9b0c1704ee2573f718e57f
Ian Firdaus <ianfirdaus31@gmail.com>
2022-02-22 14:11:45 +0700

    Added repository DI

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from 'src/tasks/tasks.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController],
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository])
+  ],
   providers: [TasksService]
 })
 export class TasksModule {}
